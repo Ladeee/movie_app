a44084d570d6bc9f4102d4015a55d2430e3470ed
OmoladeTrust <ladeakingboye@gmail.com>
2023-01-27 00:53:51 +0100

    deleted the empty index.ts file to resolve the conflicts

--- src/api/index.ts
+++
@@ -1,2 +0,0 @@
-// This folder would be responsible for the several functions we would use to make api calls to the backend
-export {};
--- src/assets/index.ts
+++
@@ -1,2 +0,0 @@
-// This folder would be responsible for the several images and assets
-export {};

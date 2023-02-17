fa77678e817aa5edba4bcdc6f1da6725a3c15df8
ibk-code <ibukunoluwarotimi123@yahoo.com>
2023-02-17 13:21:46 +0100

    :lipstick: started jobs feature design
    started working on jobs

---
+++ src/views/jobs/index.tsx
@@ -0,0 +1,19 @@
+// ------------- import external dependencies ------------
+import styled from "styled-components";
+
+// --------------- import internal dependencies   dependencies ----------------
+import LayoutHeading from "../../components/LayoutHeading";
+
+function JobsOverview() {
+  return (
+    <>
+      <LayoutHeading heading="Jobs Overview">
+        <button className="btn btn-blue">Create Job</button>
+      </LayoutHeading>
+
+      {/* -------- jobs metric summary -------- */}
+    </>
+  );
+}
+
+export default JobsOverview;

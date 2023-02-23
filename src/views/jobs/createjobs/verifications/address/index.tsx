a4e28a780606082f65e91398c1c74c09c4c7ca16
OmoladeTrust <ladeakingboye@gmail.com>
2023-02-23 17:34:11 +0100

    changed verification form to ant design

--- src/views/jobs/createjobs/verifications/address/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function AddressVerification() {
-  return <div>add</div>;
-}
--- src/views/jobs/createjobs/verifications/guarantor/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function GuarantorVerification() {
-  return <div>gua</div>;
-}
